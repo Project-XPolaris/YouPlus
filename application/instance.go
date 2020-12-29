940479a47cb725f5f283ab72733c83e4a1011ce8
AllenTom <qweasdzxczse2@gmail.com>
2020-12-29 17:58:33 +0800

    add: cors

--- application/instance.go
+++ application/instance.go
@@ -3,15 +3,16 @@ package application
 import (
 	"github.com/allentom/haruka"
 	"github.com/allentom/haruka/middleware"
+	"github.com/rs/cors"
 	"youplus/config"
 )
 
 func RunApplication() {
 	e := haruka.NewEngine()
 	e.UseMiddleware(middleware.NewLoggerMiddleware())
-
 	e.Router.GET("/apps", appListHandler)
 	e.Router.POST("/app/run", startAppHandler)
 	e.Router.POST("/app/stop", appStopHandler)
+	e.UseCors(cors.AllowAll())
 	e.RunAndListen(config.Config.Addr)
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,9 @@ module youplus
 go 1.15
 
 require (
-	github.com/allentom/haruka v0.0.0-20201223093851-f875858e397c
+	github.com/allentom/haruka v0.0.0-20201229093455-115f7b527b80
 	github.com/mitchellh/go-ps v1.0.0
+	github.com/rs/cors v1.7.0
 	github.com/rs/xid v1.2.1
 	github.com/sirupsen/logrus v1.7.0
 	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
