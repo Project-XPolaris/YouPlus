a3563155ccd81bfbaf00c46f5bab4832631b4d83
allentom <qweasdzxczse2@gmail.com>
2020-12-31 00:04:24 +0800

    add: install/uninstall service

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.15
 
 require (
 	github.com/allentom/haruka v0.0.0-20201229093455-115f7b527b80
+	github.com/jessevdk/go-flags v1.4.0
+	github.com/kardianos/service v1.2.0
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/rs/cors v1.7.0
 	github.com/rs/xid v1.2.1
--- main.go
+++ main.go
@@ -1,17 +1,102 @@
 package main
 
 import (
+	"github.com/jessevdk/go-flags"
+	srv "github.com/kardianos/service"
 	"github.com/sirupsen/logrus"
+	"os"
+	"path/filepath"
 	"youplus/application"
 	"youplus/config"
 	"youplus/service"
 )
 
-func main() {
+var svcConfig *srv.Config
+
+func initService() error {
+	workPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return err
+	}
+	svcConfig = &srv.Config{
+		Name:             "YouPlusCoreService",
+		DisplayName:      "YouPlus Core Service",
+		WorkingDirectory: workPath,
+	}
+	return nil
+}
+func Program() {
 	err := config.LoadAppConfig()
 	if err != nil {
 		logrus.Fatal(err)
 	}
 	service.LoadApps()
 	application.RunApplication()
+}
+
+type program struct{}
+
+func (p *program) Start(s srv.Service) error {
+	// Start should not block. Do the actual work async.
+	go Program()
+	return nil
+}
+
+func (p *program) Stop(s srv.Service) error {
+	// Stop should not block. Return with a few seconds.
+	return nil
+}
+
+func InstallAsService() {
+	prg := &program{}
+	s, err := srv.New(prg, svcConfig)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	s.Uninstall()
+
+	err = s.Install()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	logrus.Info("successful install service")
+}
+
+func UnInstall() {
+
+	prg := &program{}
+	s, err := srv.New(prg, svcConfig)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	s.Uninstall()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	logrus.Info("successful uninstall service")
+}
+
+var opts struct {
+	Install   bool `short:"i" long:"install" description:"Show verbose debug information"`
+	Uninstall bool `short:"u" long:"uninstall" description:"Show verbose debug information"`
+}
+
+func main() {
+	_, err := flags.ParseArgs(&opts, os.Args)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	err = initService()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	if opts.Install {
+		InstallAsService()
+		return
+	}
+	if opts.Uninstall {
+		UnInstall()
+		return
+	}
+	Program()
 }
